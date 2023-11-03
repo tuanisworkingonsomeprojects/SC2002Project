35373e269e5b59de5318e328b47d51adcf5fc704
Chelson Chong <79626294+chelsxn@users.noreply.github.com>
2023-11-03 14:01:17 +0800

    StudentMenu.java file
    Added in Display

---
+++ sce.sc2002.FinalProject/src/sce/sc2002/FinalProject/StudentMenu.java
@@ -0,0 +1,38 @@
+package sce.sc2002.FinalProject;
+
+/**
+
+ @author Chong Wen Rong, Chelson
+ @version 1.0
+ @since 11/2/2023
+*/
+
+public class StudentMenu extends Login{
+    public void display(){
+        System.out.println("--------------------------------------");
+        System.out.println("Student Camp Main");
+        System.out.println("--------------------------------------");
+        System.out.println("Choice 1 : List Opened Camps");
+        System.out.println("Choice 2 : Register for Camp");
+        System.out.println("Choice 3 : Submit Enquiries");
+        System.out.println("Choice 4 : View Regisetered Camps");
+        System.out.println("Choice 5 : Withdraw From Camp"); 
+
+    }
+
+    public void registerCamp(){
+
+    }
+
+    public void viewAvailableCamp(){
+
+    }
+
+    public void viewRegisteredCamp(){
+
+    }
+    
+    public void chooseCamp(){
+
+    }
+}
