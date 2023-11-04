6a1aff0b2fb790beae43ed7e8f6cf64db994d0c0
Chelson Chong <79626294+chelsxn@users.noreply.github.com>
2023-11-05 01:06:37 +0800

    Removing student display from campapp

--- sce.sc2002.FinalProject/src/sce/sc2002/FinalProject/CampApp.java
+++ sce.sc2002.FinalProject/src/sce/sc2002/FinalProject/CampApp.java
@@ -11,31 +11,5 @@ public class CampApp {
 		Login loginScreen = new Login();
 		loginScreen.display();
 
-		
-		//Displaying Student Menu after login
-		StudentMenu StudentLoginScreen = new StudentMenu();
-		StudentLoginScreen.StudentMenuDisplay();
-		int studentChoice;
-		Scanner sc = new Scanner(System.in);
-
-		do{
-			System.out.println("Please Select Your Choice Of Action");
-			studentChoice=sc.nextInt();
-            
-            switch(studentChoice){
-                case 1:
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-                case 4:
-                    break;
-                case 5:
-                    break;
-            }
-            
-
-        }while(studentChoice<6);
 	}
 }
